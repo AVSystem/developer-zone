82d975b6d9b78cda7c58b01e58565f502975e271
s.seget <s.seget@avsystem.com>
2021-02-26 17:42:41 +0100

    fix

--- ghpages.py
+++ ghpages.py
@@ -1,2 +1,2 @@
 from ghp_import import ghp_import
-ghp_import('site', push=True, force=True, cname="https://iotdevzone.avsystem.com")
+ghp_import('dist', push=True, force=True, cname="https://iotdevzone.avsystem.com")
