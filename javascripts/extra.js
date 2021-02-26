cc27abc6c4730d3c8a287655046e3df4c69e8869
s.seget <s.seget@avsystem.com>
2021-02-26 13:50:01 +0100

    fix

---
+++ javascripts/extra.js
@@ -0,0 +1,7 @@
+
+
+// const btnEvent1 = document.getElementById('event1');
+
+// btnEvent1.addEventListener("click", function() {
+//     ga('send', 'event', 'button click', 'button click', 'button click', ); 
+// });
