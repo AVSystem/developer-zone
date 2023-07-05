8744281afec0a33c62ddabcaa8ca5111d46920cc
kpieczonka <k.pieczonka@avsystem.com>
2023-07-05 11:38:51 +0200

    add topLinePageInfo, fix sticky menu

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -17,3 +17,43 @@
 // btnEvent1.addEventListener("click", function() {
 //     ga('send', 'event', 'button click', 'button click', 'button click', );
 // });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Funkcja, która będzie wykonywana po załadowaniu drzewa DOM
+//   function changeElementTopStyle() {
+//   var mbTabs = document.querySelector('.md-tabs');
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//
+//   console.log(mbTabs)
+//   console.log(scrollTop)
+//
+//
+//   if (scrollTop > 0) {
+//     mbTabs.style.top = '55px'; // Zmienić na żądany styl
+//   } else {
+//     mbTabs.style.top = '110px'; // Usunąć styl, jeśli scrollTop jest mniejsze lub równe 0
+//   }
+// }
+//
+//
+//
+//  window.addEventListener('scroll', changeElementTopStyle);
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  function adjustTabsPosition() {
+    var mbTabs = document.querySelector('.md-tabs');
+    var headerNav = document.querySelector('.md-header-nav');
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > headerNav.offsetHeight) {
+      mbTabs.style.top = headerNav.offsetHeight - 10 + 'px';
+    } else {
+      mbTabs.style.top = '';
+    }
+  }
+
+  window.addEventListener('resize', adjustTabsPosition);
+  window.addEventListener('scroll', adjustTabsPosition);
+});
