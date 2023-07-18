722aa5dc11d10b34f4504e84d20f9edb5069169c
Mieszko Mieruński <m.mierunski@avsystem.com>
2023-07-18 17:30:45 +0200

    Fix image generation.

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 from PIL import Image, ImageDraw, ImageFont
+import hashlib
+import os
+from mkdocs.structure.files import File
 
 
 def define_env(env):
 
-    image_number = 0
     @env.macro
     def console(description, txt):
-        nonlocal image_number
-        file_path = 'assets/images/console_gen_' + str(image_number) + '.png'
-        print(env.page)
+        # Check if image exists already and has the same hash, if so, return the image
+        hash = description + txt
+        hash = hashlib.md5(hash.encode('utf-8')).hexdigest()
+        file_path = 'images/console_gen_' + hash + '.png'
+        p = os.path.dirname(env.page.url).split("/")
+        p = p[:-1]
+        relative_path = os.path.join(*p, file_path)
+        p = os.path.join("docs", *p, file_path)
+
+        documentation_image_path = '![' + description + \
+            '](' + file_path + " \"" + description + "\")"
+        if os.path.isfile(p):
+            return documentation_image_path
+
         font_size = 16
 
-        mono_font = ImageFont.truetype("DejaVuSansMono.ttf", font_size, index=0)
-        img = Image.new('RGB', (10, 10), color = (0, 0, 0))
+        mono_font = ImageFont.truetype(
+            "DejaVuSansMono.ttf", font_size, index=0)
+        img = Image.new('RGB', (10, 10), color=(0, 0, 0))
         draw = ImageDraw.Draw(img)
-        (_, _, width, height) = draw.multiline_textbbox((0, 0), txt, font=mono_font, align="left")
+        (_, _, width, height) = draw.multiline_textbbox(
+            (0, 0), txt, font=mono_font, align="left")
         img = img.resize((width + 20, height + 20))
         draw = ImageDraw.Draw(img)
         draw.fontmode = "0"
-        draw.text((10, 10), txt, fill =(255, 255, 255), font = mono_font)
-        img.save("overrides/" + file_path)
-        img.save("site/" + file_path)
-        image_number += 1
-        return '!['+ description + '](/' + file_path +  " \"" + description + "\")"
+        draw.text((10, 10), txt, fill=(255, 255, 255), font=mono_font)
+        img.save(p)
+        print("Generated image: " + p)
+
+        with open("temp.txt", "a") as f:
+            f.write(relative_path + "\n")
+        return documentation_image_path
+
+
+def on_page_markdown(_, page, config, files):
+    if os.path.exists("temp.txt"):
+        with open("temp.txt", "r") as f:
+            for line in f:
+                file = File(path=line.strip(), src_dir=config.docs_dir,
+                            dest_dir=config.site_dir, use_directory_urls=False)
+                files.append(file)
+
+        # Clear temp file
+        os.remove("temp.txt")
